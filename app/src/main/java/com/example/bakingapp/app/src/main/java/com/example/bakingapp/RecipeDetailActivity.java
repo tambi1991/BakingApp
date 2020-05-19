88bacec17d915be51ac37e7e27e71843cfaf314d
tambi1991 <65135831+tambi1991@users.noreply.github.com>
2020-05-20 00:15:32 +0200

    Add files via upload

---
+++ app/src/main/java/com/example/bakingapp/app/src/main/java/com/example/bakingapp/RecipeDetailActivity.java
@@ -0,0 +1,32 @@
+package com.example.bakingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.bakingapp.model.Recipe;
+
+public class RecipeDetailActivity extends AppCompatActivity {
+    // variable for the Recipe
+    private Recipe mRecipe;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_detail);
+        //intent that started this activity
+        Intent intentThatStartedThisActivity = getIntent();
+        if (intentThatStartedThisActivity == null) {
+            closeOnError();
+        }
+        mRecipe = intentThatStartedThisActivity.getParcelableExtra("Recipe");
+
+    }
+
+    private void closeOnError() {
+        finish();
+        Toast.makeText(this, " no image available", Toast.LENGTH_SHORT).show();
+    }
+}
